dd215a8bb256211aa019a862f5b95aa7754cec86
Juju <Julienbrouzes@hotmail.com>
2025-11-06 22:22:31 +0100

    add window exe

--- commands.sh
+++ commands.sh
@@ -1,2 +1,5 @@
 pyinstaller --onefile main.py
 #pyinstaller --onefile --windowed main.py
+
+
+C:\Users\Juju\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\Scripts\pyinstaller.exe --onefile main.py
